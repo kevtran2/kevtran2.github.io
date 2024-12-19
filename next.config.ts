060c0041f07818e95a7ebb5a84e19b5a2438d890
kevtran2 <kevin.tran.675@gmail.com>
2024-12-19 10:59:13 -0800

    Update to reflect special repository
    No need for basepath, assetprefiex, or homepage with special repo

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export", // Enable static site export
-  ...(isGithubPages && {
-    basePath: "/kevtran2.github.io", // GitHub Pages base path
-    assetPrefix: "/kevtran2.github.io", // Asset prefix for GitHub Pages
-  }),
 };
 
 export default nextConfig;
