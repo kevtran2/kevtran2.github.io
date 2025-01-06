a5cdb783333a15b4c8d7dd00dd6a7c3da5b0784a
kevtran2 <kevin.tran.675@gmail.com>
2025-01-06 13:21:36 -0800

    Deployed site missing CSS fix
    Fixed by adding removing Jekyll from running on Github Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enable static site export
-  assetPrefix: '/kevtran2.github.io/',
+  //basePath: process.env.NODE_ENV === 'production' ? '/kevtran2.github.io' : '',
+  //assetPrefix: process.env.NODE_ENV === 'production' ? '/kevtran2.github.io/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 export default nextConfig;
